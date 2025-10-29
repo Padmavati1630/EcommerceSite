e46424fc99b2e6ca144639e81a536bbc5aab327e
padma <padmashivanagutti90@gmail.com>
2025-10-29 14:16:53 +0530

    3.Added Swagger

---
+++ src/main/java/com/jsp/clickNBuy/contoller/GeneralController.java
@@ -0,0 +1,12 @@
+package com.jsp.clickNBuy.contoller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GeneralController {
+	@GetMapping("/")
+	public String loadSwagger() {
+		return "redirect:swagger-ui.html";
+	}
+}
